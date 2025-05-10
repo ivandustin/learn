6e699ca9da26abe7efda401188cfb8420b0d3212
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-05-10 11:00:45 +0800

    Test embeds

--- learn/modules/embeds.py
+++ learn/modules/embeds.py
@@ -6,5 +6,5 @@ class Embeds(Module):
     def __init__(self, width: int, lengths: list[int], rngs: Rngs):
         self.embeds = [Embed(n, width, rngs=rngs) for n in lengths]
 
-    def __call__(self, x):
-        return array([embed(x) for embed in self.embeds])
+    def __call__(self, xs):
+        return array([self.embeds[i](x) for i, x in enumerate(xs)])
