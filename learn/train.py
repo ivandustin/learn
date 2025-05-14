73d0d8c668aa7179644b855a650fe78337aeb140
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2025-05-14 20:51:55 +0800

    Switch train arg

--- learn/train.py
+++ learn/train.py
@@ -3,7 +3,7 @@ from flax.nnx import jit, value_and_grad, vmap
 
 
 @jit
-def train(model, optimizer, x, y):
+def train(optimizer, model, x, y):
     def loss(model):
         yhat = vmap(model)(x)
         return softmax_cross_entropy_with_integer_labels(yhat, y).mean()
